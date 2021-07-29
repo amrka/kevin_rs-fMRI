1aeb56d210ac77db25bac450f8a10d56884923a9
amrka <amr.kamel@uv.es>
2021-07-29 15:29:39 +0200

    create s rs study template

---
+++ creating_rs_template_00002.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# create a template from functional images to use for antsBrainextraction
+# I did not create one for antamoical images, since the result was not very good, also BET works well
