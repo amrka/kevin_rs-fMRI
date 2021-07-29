6c98a9dc4409eedf95ecaf7021d13104c6b733bd
amrka <amr.kamel@uv.es>
2021-07-29 15:33:00 +0200

    finish the script

--- creating_rs_template_00002.sh
+++ creating_rs_template_00002.sh
@@ -2,3 +2,25 @@
 
 # create a template from functional images to use for antsBrainextraction
 # I did not create one for antamoical images, since the result was not very good, also BET works well
+
+
+mkdir /Volumes/Amr_1TB/Kevin/rs-temp
+
+imcp \
+/Volumes/Amr_1TB/Kevin/raw_data_bids/A031120/func/sub-A031120_X8_bold.nii.gz \
+/Volumes/Amr_1TB/Kevin/rs-temp/
+
+cd /Volumes/Amr_1TB/Kevin/rs-temp
+# use only 50 volumes out of 900
+fslroi sub-A031120_X8_bold.nii.gz  sub 450 50
+imrm sub-A031120_X8_bold.nii.gz
+fslsplit sub.nii.gz -t
+
+imrm sub.nii.gz
+
+buildtemplateparallel.sh \
+-d 3 \
+-c 5 \
+-j 200 \
+-r 1 \
+-o epi_templ vol*.nii.gz
