015e4a7a40a399644f6ac5a31713a4333d9c5303
amr <amr.kamel@uv.es>
2021-08-01 15:07:23 +0200

    change dir for ubuntu

--- add_run_number_00004.sh
+++ add_run_number_00004.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd /Volumes/Amr_1TB/Kevin/raw_data_bids
+cd /media/amr/Amr_4TB/Work/Kevin/raw_data_bids
 
 for folder in *;do
     cd $folder
     cd func
     echo $folder
-    python3 /Volumes/Amr_1TB/Kevin/kevin_rs-fMRI/add_run_number_00003.py .
+    python3 /media/amr/Amr_4TB/Work/Kevin/kevin_rs-fMRI/add_run_number_00003.py .
 
     cd ..
     cd ..
