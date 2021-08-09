89fb471c97e644fa9d9c4eef9f33b5d9db9f148a
amrka <amr.kamel@uv.es>
2021-08-09 17:44:03 +0200

    fix the script to display melodic scene

--- display_melodic_for_classification_00008.sh
+++ display_melodic_for_classification_00008.sh
@@ -1,57 +1,28 @@
+#!bin/bash
+# you pass the Kevin dir parent, the run number as (run-01 or run-02), and the subject number
+# >>> display_melodic_for_classification_00008.sh /Volumes/Amr_1TB run-01 A251120
+
+Usage() {
+    echo ""
+    echo "you pass the Kevin dir parent, the run number as (run-01 or run-02), and the subject number"
+    echo "Usage: >>> display_melodic_for_classification_00008.sh /Volumes/Amr_1TB run-01 A251120"
+    echo ""
+    exit 1
+}
+
 fsleyes \
---scene lightbox \
---worldLoc -5.000247955322266 1.355571746826186 5.000247955322266 \
---displaySpace /Volumes/Amr_1TB/Kevin/resting_state_preproc_func_workingdir/resting_fmri_preproc_func/_run_id_run-01_subject_id_A021120/brain_extraction_roi/highres001_BrainExtractionBrain.nii.gz \
---zaxis 2 \
+--autoDisplay \
+--scene melodic \
+--hideCursor \
+--displaySpace world  \
+--zaxis 1 \
 --sliceSpacing 5.0 \
---zrange -2.500008471547114 197.49950417492667 \
---ncols 9 \
---nrows 3 \
---bgColour 0.0 0.0 0.0 \
---fgColour 1.0 1.0 1.0 \
---cursorColour 0.0 1.0 0.0 \
---colourBarLocation top \
---colourBarLabelSide top-left \
---colourBarSize 100.0 \
---labelSize 12 \
---performance 3 \
---movieSync /Volumes/Amr_1TB/Kevin/resting_state_preproc_func_workingdir/resting_fmri_preproc_func/_run_id_run-01_subject_id_A021120/brain_extraction_roi/highres001_BrainExtractionBrain.nii.gz \
---name "highres001_BrainExtractionBrain" \
---overlayType volume \
---alpha 100.0 \
---brightness 50.0 \
---contrast 50.0 \
---cmap greyscale \
---negativeCmap greyscale \
---displayRange 0.0 15.264443397521973 \
---clippingRange 0.0 15.417087831497192 \
---modulateRange 0.0 15.264443397521973 \
---gamma 0.0 \
---cmapResolution 256 \
---interpolation none \
---numSteps 100 \
---blendFactor 0.1 \
---smoothing 0 \
---resolution 100 \
---numInnerSteps 10 \
---clipMode intersection \
---volume 0 /Volumes/Amr_1TB/Kevin/resting_state_preproc_func_workingdir/resting_fmri_preproc_func/_run_id_run-01_subject_id_A021120/_blurfwhm_bx_by_bz_6.5.6.5.6.5/_dim_25/Melodic/melodic.ica/melodic_IC.nii.gz \
---name "melodic_IC" \
---overlayType volume \
---alpha 100.0 \
---cmap red-yellow \
---negativeCmap blue-lightblue \
---useNegativeCmap \
---displayRange 2.0 10.0 \
---clippingRange 2.0 65.92513160705566 \
---modulateRange 0.0 65.27240753173828 \
---gamma 0.0 \
---cmapResolution 256 \
---interpolation none \
---numSteps 100 \
---blendFactor 0.1 \
---smoothing 0 \
---resolution 100 \
---numInnerSteps 10 \
---clipMode intersection \
---volume 1
+--zrange -98.64418029785156 101.3553237915039 \
+--ncols 10 \
+--nrows 4   \
+${1}/Kevin/resting_state_preproc_func_workingdir/resting_fmri_preproc_func/_run_id_${2}_subject_id_${3}/_blurfwhm_bx_by_bz_6.5.6.5.6.5/_dim_25/Melodic/melodic.ica
+
+
+kevin dir
+run-01
+subject name
