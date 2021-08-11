340696d84351feb27ce0e665404362e3921b531d
amrka <amr.kamel@uv.es>
2021-08-11 13:32:23 +0200

    copy ICA labels to seperate folders for easy access

---
+++ copy_ICA_labels_00009.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# copy the labels txt files from inside the original directory after manual classification to
+# an outside directory for convenience
+
+# you pass the Kevin dir parent
+# >>> copy_ICA_labels_00009.sh /Volumes/Amr_1TB
+
+Usage() {
+    echo ""
+    echo "you pass the Kevin dir parent"
+    echo ""
+    echo "Usage:"
+    echo ">>> copy_ICA_labels_00009.sh /Volumes/Amr_1TB"
+    echo ""
+    exit 1
+}
+
+[ "$1" = "" ] && Usage
+
+
+cd ${1}/Kevin
+mkdir ICA_labels
+
+cd ${1}/Kevin/resting_state_preproc_func_workingdir/resting_fmri_preproc_func
+cp _run_id_run-0?_subject_id_*/_blurfwhm_bx_by_bz_6.5.6.5.6.5/_dim_25/Melodic/melodic.ica/*_run-0?.txt   ../../ICA_labels/
