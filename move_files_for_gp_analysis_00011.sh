aa715f65e41d57faec34b095b6782ab005238028
amrka <amr.kamel@uv.es>
2021-08-19 16:05:17 +0200

    create lists for DR with run number

--- move_files_for_gp_analysis_00011.sh
+++ move_files_for_gp_analysis_00011.sh
@@ -58,4 +58,22 @@ python3 ${1}/Kevin/kevin_rs-fMRI/change_files_to_contain_gp_name.py ${1}/Kevin/r
 ls ${1}/Kevin/resting_state_gp_analysis_ants/*.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_ants/melodic_list_ants.txt
 ls ${1}/Kevin/resting_state_gp_analysis_flirt/*.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_flirt/melodic_list_flirt.txt
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# some of the subjects included in melodic analysis, but were excluded from the dual regression
+# only those who have the prefix A_* or B_* will be included in the analysis
+# ants
+ls ${1}/Kevin/resting_state_gp_analysis_ants/A_*_run-01_ants.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_ants/DR_list_ants_run_01.txt
+ls ${1}/Kevin/resting_state_gp_analysis_ants/B_*_run-01_ants.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_ants/DR_list_ants_run_01.txt
+
+ls ${1}/Kevin/resting_state_gp_analysis_ants/A_*_run-02_ants.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_ants/DR_list_ants_run_02.txt
+ls ${1}/Kevin/resting_state_gp_analysis_ants/B_*_run-02_ants.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_ants/DR_list_ants_run_02.txt
+
+# flirt
+ls ${1}/Kevin/resting_state_gp_analysis_flirt/A_*_run-01_flirt.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_flirt/DR_list_flirt_run_01.txt
+ls ${1}/Kevin/resting_state_gp_analysis_flirt/B_*_run-01_flirt.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_flirt/DR_list_flirt_run_01.txt
+
+ls ${1}/Kevin/resting_state_gp_analysis_flirt/A_*_run-02_flirt.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_flirt/DR_list_flirt_run_02.txt
+ls ${1}/Kevin/resting_state_gp_analysis_flirt/B_*_run-02_flirt.nii.gz >> ${1}/Kevin/resting_state_gp_analysis_flirt/DR_list_flirt_run_02.txt
+
+
 #then you create the design
