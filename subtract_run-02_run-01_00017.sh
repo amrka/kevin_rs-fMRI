512ffb57b3997cdc70401232d47900197cb5fb5c
amrka <amr.kamel@uv.es>
2021-08-27 18:39:58 +0200

    loop over all the images of all dims

--- subtract_run-02_run-01_00017.sh
+++ subtract_run-02_run-01_00017.sh
@@ -11,6 +11,8 @@ Usage() {
     echo ""
     echo "Usage:"
     echo ">>> ./subtract_run-02_run-01_00017.sh /Volumes/Amr_1TB"
+    echo ">>> ./subtract_run-02_run-01_00017.sh /home/in/aeed/Work"
+    echo ">>> ./subtract_run-02_run-01_00017.sh /media/amr/Amr_4TB/Work"
     echo ""
     exit 1
 }
@@ -23,5 +25,24 @@ mkdir -p ${1}/Kevin/subtraction/{_dim_10,_dim_15,_dim_20,_dim_25,_dim_40,_dim_50
 
 
 # run-02 ->'43ddee1b109476bbf41ba6bf431ef9eefb2055bd'
-                 
+
 # run-01 ->'d0f02a8721eff087f281113015c672685b8a198b'
+
+# N.B I checked that fslmaths subtraction works on all volumes of 4D images
+for img in ${1}/Kevin/resting_state_melodic/melodic_workflow/_subject_id_ants/_dim_*/43ddee1b109476bbf41ba6bf431ef9eefb2055bd/dual_regression/output/dr_stage2_ic*.nii.gz;do
+    # extract the dim instead of looping over all of them or worse, copying the code many times
+    dim=`echo $img | sed s?\$1/Kevin/resting_state_melodic/melodic_workflow/_subject_id_ants/_dim_?? | sed s?/43ddee1b109476bbf41ba6bf431ef9eefb2055bd/dual_regression/output/.*??`
+    echo $dim
+
+    echo $img
+
+    echo "${1}/Kevin/resting_state_melodic/melodic_workflow/_subject_id_ants/_dim_${dim}/d0f02a8721eff087f281113015c672685b8a198b/dual_regression/output/`basename ${img}`"
+
+    fslmaths \
+    ${img} \
+    -sub \
+    ${1}/Kevin/resting_state_melodic/melodic_workflow/_subject_id_ants/_dim_${dim}/d0f02a8721eff087f281113015c672685b8a198b/dual_regression/output/`basename ${img}` \
+    ${1}/Kevin/subtraction/_dim_${dim}/subtraction_dim_${dim}_`basename ${img}`
+
+    echo "${1}/Kevin/subtraction/_dim_${dim}/subtraction_dim_${dim}_`basename ${img}`"
+done
