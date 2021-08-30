369abd488533a0f9306df80f5d60d4ff3d1ee0fd
amrka <amr.kamel@uv.es>
2021-08-30 12:16:42 +0200

    fix parent dir

--- create_subtract_PALM_directories_00018.sh
+++ create_subtract_PALM_directories_00018.sh
@@ -29,49 +29,49 @@ mkdir -p palm_ants/subtract/{dim_10,dim_15,dim_20,dim_25,dim_40,dim_50,dim_100}
 # ==================================================================================================
 # generate lists with -i flag to use with palm instead of inputting manually
 # 10
-for img in `ls ${1}/subtraction/_dim_10/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_10/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_10/palm_dim_10_subtract.txt;
 done
 
 # =============================================15=============================================
 # 15
-for img in `ls ${1}/subtraction/_dim_15/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_15/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_15/palm_dim_15_subtract.txt;
 done
 
 # =============================================20=============================================
 # 20
-for img in `ls ${1}/subtraction/_dim_20/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_20/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_20/palm_dim_20_subtract.txt;
 done
 
 # =============================================25=============================================
 # 25
-for img in `ls ${1}/subtraction/_dim_25/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_25/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_25/palm_dim_25_subtract.txt;
 done
 
 # =============================================40=============================================
 # 40
-for img in `ls ${1}/subtraction/_dim_40/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_40/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_40/palm_dim_40_subtract.txt;
 done
 
 # =============================================50=============================================
 # 50
-for img in `ls ${1}/subtraction/_dim_50/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_50/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_50/palm_dim_50_subtract.txt;
 done
 
 # =============================================100=============================================
 # 100
-for img in `ls ${1}/subtraction/_dim_100/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
+for img in `ls ${1}/Kevin/subtraction/_dim_100/subtraction_dim_*_dr_stage2_ic*.nii.gz`;do
 
     echo "-i $img" >> ${1}/Kevin/palm_ants/subtract/dim_100/palm_dim_100_subtract.txt;
 done
