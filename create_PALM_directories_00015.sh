c3b1f5a0bfdd6dbe81be2188d91a5639e18cb2bb
amr <amr.kamel@uv.es>
2021-10-01 14:15:58 -0400

    delete folder if existed

--- create_PALM_directories_00015.sh
+++ create_PALM_directories_00015.sh
@@ -23,6 +23,8 @@ Usage() {
 
 cd ${1}/Kevin
 
+rm -r palm_ants
+
 mkdir palm_ants
 mkdir -p palm_ants/run-01/{dim_10,dim_15,dim_20,dim_25,dim_40,dim_50,dim_100}
 mkdir -p palm_ants/run-02/{dim_10,dim_15,dim_20,dim_25,dim_40,dim_50,dim_100}
